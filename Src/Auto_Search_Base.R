e9b517292f7221663ca694b8dc9c4d8b2127ccd6
“z” <u6015364@anu.edu.au>
2018-09-13 16:04:43 +1000

    Update

---
+++ Src/Auto_Search_Base.R
@@ -0,0 +1,97 @@
+
+merge_string_list <- function(string_list, sep){
+  
+  if(length(string_list) <= 1){
+    return(string_list)
+  }
+  
+  merged_string = string_list[1]
+  for(i in 2:length(string_list)){
+    merged_string = paste(merged_string, string_list[i], sep = sep)
+  }
+  return(merged_string)
+}
+
+save_df <- function(rt, save_name){
+  
+  newrt = data.frame(rt['user_id'])
+  for(col_name in colnames(rt)){
+    
+    if(typeof(rt[[col_name]]) == "list"){
+      column <- sapply(rt[[col_name]], function(x){
+        return(merge_string_list(x, " "))
+      })
+      newrt[,col_name] = column
+    }
+    else{
+      newrt[,col_name] = rt[,col_name]
+    }
+    
+  }
+  write.csv(newrt, file = save_name, row.names = F)
+}
+
+
+auto_search_tweets <- function(query, search_amount_once, search_interval, total_duration){
+  
+  # Get script start time.
+  start_time = Sys.time()
+  rt = search_tweets(
+    query, n = search_amount_once, include_rts = FALSE
+  )
+  
+  # Select tweet created during the show.
+  inDuration = which(rt$created_at >= start_time)
+  tweet_df = rt[inDuration,]
+  previous_tweet_amount = nrow(tweet_df)
+  
+  Sys.sleep(search_interval)
+  
+  while (TRUE) {
+    
+    # Search tweets
+    rt = search_tweets(
+      query, n = search_amount_once, include_rts = FALSE
+    )
+    inDuration = which(rt$created_at >= start_time)
+    rt = rt[inDuration,]
+    
+    # Combine two tweet data frame
+    tweet_df = rbind(tweet_df, rt)
+    raw_rows = nrow(tweet_df)
+    tweet_df = unique(tweet_df)
+    unique_rows = nrow(tweet_df)
+    
+    # No new tweets found, increase the search step.
+    if(unique_rows - previous_tweet_amount <= 0){
+      search_interval = search_interval * 2
+    }
+    
+    # Searched tweets are all new, decrease the search step.
+    if(unique_rows - previous_tweet_amount >= search_amount_once){
+      search_interval = search_interval / 2
+    }
+    previous_tweet_amount = unique_rows
+    print(previous_tweet_amount)
+    print(search_interval)
+    
+    now_time = Sys.time()
+    if(now_time - start_time > total_duration){
+      
+      # Save csv
+      file_name = paste(query, start_time, total_duration, sep = "_")
+      file_name = paste("../Data/", file_name, ".csv", sep = "")
+      save_df(tweet_df, file_name)
+      
+      break
+    }
+    
+    Sys.sleep(search_interval)
+  }
+  
+}
+
+
+### Test
+
+# auto_search_tweet("a", 2, 3, 30)
---
+++ Src/Question_Time.R
@@ -0,0 +1,24 @@
+library(rtweet)
+
+#Create tweet API token
+create_token(
+  app = "my_twitter_research_app",
+  consumer_key = "G3MQONk4InSpaEbLEEii83NeR",
+  consumer_secret = "sjhQId8edr4nSkccyDiwJwogA6vqHoGDkqGSnTXNCnlQ8kpvkM",
+  access_token = "875178186383835137-iVunpJKrBqGdP9o59zICU20wzU4mwyD",
+  access_secret = "ypOqFVOYtuwbXQZ9875iKUAZ7ijtRATFPbnZ6GgnC7MhL")
+
+# Load function
+source("./Auto_Search_Base.R")
+
+# Set paremeters
+query = "#bbcqt"
+search_amount_once = 1500
+search_interval = 300
+total_duration = as.difftime(90, units = "mins")
+
+# Run search function
+auto_search_tweets(query = query, 
+                  search_amount_once = search_amount_once,
+                  search_interval = search_interval,
+                  total_duration = total_duration)
